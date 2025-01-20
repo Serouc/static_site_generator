86743327b4cd06af4cb572329fb29052622e83f3
Srouc <scscourtenay@gmail.com>
2025-01-20 21:21:42 +1100

    added mardown extraction for links and images

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,30 @@
+import unittest
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        input = extract_markdown_images(text)
+        result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(input, result)
+    
+    def test_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        input = extract_markdown_links(text)
+        result = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(input, result)
+
+    def test_mix(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        input = [extract_markdown_images(text),extract_markdown_links(text)]
+        result = [[("rick roll", "https://i.imgur.com/aKaOqIh.gif")], [("to youtube", "https://www.youtube.com/@bootdotdev")]]
+        self.assertEqual(input, result)
+
+    def test_invalid_alt_text(self):
+        text = "This is text with a ![rick roll[with nested alt brackets]](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        input = extract_markdown_images(text)
+        result = [("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(input, result)
+
+if __name__ == "__main__":
+    unittest.main()
