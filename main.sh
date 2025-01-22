22880f729b34a5160315cf54b9f7c6f54c86624b
Srouc <scscourtenay@gmail.com>
2025-01-22 20:48:22 +1100

    added page generation

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-python3 src/main.py
+cd "$(dirname "$0")" && python3 src/main.py
+cd public && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -2,12 +2,19 @@ from textnode import TextNode, TextType
 from parentnode import ParentNode
 from leafnode import LeafNode
 from htmlnode import HTMLNode
+from markdown_to_html import markdown_to_html_node
+from extract_markdown import extract_title
 import os, shutil
 
 def main():
-    new_textnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(new_textnode.__repr__())
     static_to_public()
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root =  os.path.dirname(script_dir)
+    public_dir = os.path.join(project_root, "public")
+    content_dir = os.path.join(project_root, "content")
+
+    generate_page(os.path.join(content_dir, "index.md"),os.path.join(project_root, "template.html"), os.path.join(public_dir, "index.html"))
 
 
 def static_to_public():
@@ -32,8 +39,21 @@ def copy_static_to_public(working_dir, public_dir):
             os.mkdir(os.path.join(public_dir, object))
             copy_static_to_public(os.path.join(working_dir, object), os.path.join(public_dir, object))
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        markdown = f.read()
+    with open(template_path) as t:
+        html_string = t.read()
+        print(html_string)
+        
+    title = extract_title(markdown)
+    content = markdown_to_html_node(markdown).to_html()
+    html_string = html_string.replace("{{ Title }}", title).replace("{{ Content }}", content)
+
+    with open(dest_path, "w") as d:
+        d.write(html_string)
 
 
-    
 
 main()
